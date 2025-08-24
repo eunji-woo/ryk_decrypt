10057a1271ddab26dd3b7995b8badb0da393498d
eunji <eunji20392@naver.com>
2025-08-24 20:22:55 +0900

    FLAG.txt.ryk만 복호화하는 버전

--- dec.py
+++ dec.py
@@ -1,24 +1,45 @@
+import os
 import argparse
+from Crypto.Cipher import AES
+
+def read_key_hex_or_raw(path):
+    with open(path, "r", encoding="utf-8") as f:
+        s = f.read()
+        s = bytes.fromhex(s)
+    return s
+
+
+def decrypt_aes_gcm(data: bytes, key: bytes):
+    if len(data) < 29:
+        raise ValueError(".복호화 대상 X")
+    nonce = data[:12]
+    tag = data[-16:]
+    ct = data[12:-16]
+    cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
+    pt = cipher.decrypt_and_verify(ct, tag)
+    return pt
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--key", required=True, help="AES 키 파일 경로")
-    parser.add_argument(
-        "--root",
-        default=r"C:\Users\Alpha\Desktop",
-        help="탐색 시작 폴더 (기본: 사용자 바탕화면)"
-    )
-    args = parser.parse_args()
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--key", required=True, help=".aes 키 파일 경로")
+    ap.add_argument("--root", required=True, help="탐색 시작 폴더 (기본: 사용자 바탕화면)")
+    args = ap.parse_args()
 
-    key_path = args.key
-    root_path = args.root
+    key = read_key_hex_or_raw(args.key)
+    root = args.root
 
-    # 키 파일 읽기
-    with open(key_path, "rb") as f:
-        a = f.read()
+    src = os.path.join(root, "FLAG.txt.ryk")
+    dst = os.path.join(root, "FLAG.txt")
 
-    print(f"AES 키 파일 내용: {a.decode('utf-8')}")
-    print(f"root 경로: {root_path}")
+  
+    with open(src, "rb") as f:
+        data = f.read()
+    pt = decrypt_aes_gcm(data, key)
+
+    with open(dst, "wb") as f:
+        f.write(pt)
+
+    os.remove(src)
 
 if __name__ == "__main__":
     main()
